e57a0930f8845ea21c0b89e852a797081f351009
Vandy225 <vandenhoek123@live.com>
2018-03-09 15:30:44 -0700

    added ArmyView and StatView activities, set layout information for each

---
+++ app/src/main/java/com/example/josh/betterscribe/StatView.java
@@ -0,0 +1,13 @@
+package com.example.josh.betterscribe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat_view);
+    }
+}
